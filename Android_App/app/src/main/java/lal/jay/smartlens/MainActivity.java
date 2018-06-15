d31bf23209fd396bcccbbba16d0cd760762416d8
thejaelal <jay.lal@somaiya.edu>
2018-06-15 13:02:45 +0530

    Added a launch button with toast

--- Android_App/app/src/main/java/lal/jay/smartlens/MainActivity.java
+++ Android_App/app/src/main/java/lal/jay/smartlens/MainActivity.java
@@ -2,6 +2,9 @@ package lal.jay.smartlens;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launch(View v) {
+
+        Log.i("launch","Launch Button Clicked");
+        Toast.makeText(getApplicationContext(), "Launching", Toast.LENGTH_SHORT).show();
+
     }
 }
