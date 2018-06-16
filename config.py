4b5e9b8ed7e339b2c8863f2c289a5573974e9e95
thejaelal <jay.lal@somaiya.edu>
2018-06-16 09:16:58 +0530

    Added Realtime Detection from IP Webcam

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 #Parameters
 threshold_score = 0.20
 
+#Stream Url
+stream_url = "http://192.168.43.1:8080/shot.jpg"
+
 #Relative Paths...
 path_to_infer_graph = "model/frozen_inference_graph.pb"
 test_imgs_dir = 'Test_Images'
