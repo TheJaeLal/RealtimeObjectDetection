9e5f964585e4cf50ec8355e54bf1195a91af52c4
thejaelal <jay.lal@somaiya.edu>
2018-07-06 21:47:59 +0530

    Added Volley Asynchronous Networking, Objects detected and segmented successfully

--- PicRead/app/src/main/java/lal/jay/picread/MainActivity.java
+++ PicRead/app/src/main/java/lal/jay/picread/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -13,21 +15,40 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private Button detectButton;
+
+//    private static String serverUrl = "http://35.200.202.208:5000/";
+
+    private static String serverUrl = "http://192.168.43.147:5000/";
 
     private static final int REQUEST_CAMERA_PERMISSION_RESULT = 0;
 
@@ -117,6 +138,90 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void detectObjects(View view) {
+
         Log.d("OnClick","Detect Button Clicked...");
+
+        //Get ImageView Content as Bitmap!
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+//        String response = uploadImage(bitmap2String(bitmap));
+
+        uploadImage(bitmap2String(bitmap));
+
+//        if (response!=null) {
+//            Log.d("Network", "Response received");
+//
+//            Bitmap resultImage = string2Bitmap(response);
+//
+//            imageView.setImageBitmap(resultImage);
+//        }
+//        else{
+//            Log.d("Network","Error! Null response");
+//        }
     }
+
+    public void uploadImage(final String image)
+    {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, serverUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Network", "Response received");
+
+                Bitmap resultImage = string2Bitmap(response);
+
+                imageView.setImageBitmap(resultImage);
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Network", "Erroneous Response!");
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("name","default_name.jpg");
+                params.put("image",image);
+//                uploaded = true;
+                //return super.getParams();
+                return params;
+            }
+
+            @Override
+            public Priority getPriority() {
+                return Priority.IMMEDIATE;
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                20000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        MySingleton.getInstance(MainActivity.this).addToRequestQueue(stringRequest);
+
+    }
+
+    public String bitmap2String(Bitmap bitmap)
+    {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,40,byteArrayOutputStream);
+
+        byte[] imgBytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imgBytes, Base64.DEFAULT);
+    }
+
+    private Bitmap string2Bitmap(String encodedImage)
+    {
+        byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+
+        return decodedByte;
+    }
+
+
 }
