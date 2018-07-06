910fc27907a09c870c21eb837f815d1fd553970d
thejaelal <jay.lal@somaiya.edu>
2018-07-06 09:56:41 +0530

    New Static App:Added Fullscreen support to the

--- Object_Detection_Segmentation_App/app/src/main/java/lal/jay/picsmart/MainActivity.java
+++ Object_Detection_Segmentation_App/app/src/main/java/lal/jay/picsmart/MainActivity.java
@@ -2,6 +2,7 @@ package lal.jay.picsmart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        //Get the decorView
+        View decorView = getWindow().getDecorView();
+
+        //If the app is brought into focus..
+        if(hasFocus)
+        {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+
     }
 }
